b0115997b41396f56573c055480c5d119b438d15
Hani Mounla <hani.mounla@outlook.com>
2019-09-02 00:50:28 +0300

    Renamed main form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -257,7 +257,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::HexabitzScale.Properties.Resources.logo;
+            this.pictureBox1.Image = global::USBScaleSoftware.Properties.Resources.logo;
             this.pictureBox1.Location = new System.Drawing.Point(743, 525);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 50);
